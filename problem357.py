052d6ff8a057747475b9375d60c81e46cf8f3fe1
Rahul sahu <43337390+rahulsa123@users.noreply.github.com>
2019-06-08 19:50:29 +0530

    Create problem357.py

---
+++ problem357.py
@@ -0,0 +1,28 @@
+#takes more >1min 
+import time
+prime=[0]*100000000
+p=2 
+su =0
+t=time.time()
+while(p*p<100000000):
+    if(prime[p]==1):
+        for i in range(2,int((p-1)**0.5)+1):
+            if((p-1)%i==0):
+                if not(prime[(i+(p-1)//i)]==1):
+                    break
+        else:
+            su+=p-1
+           # print(p-1)
+        for i in range(p+p,100000000,p):
+            prime[i]=0
+    p+=1
+for j in range(p+1,100000000-2):
+    if(prime[j+1]==1):
+        for i in range(2,int((j)**0.5)+1):
+            if((j)%i==0):
+                if not(prime[(i+(j)//i)]==1):
+                    break
+        else:
+            su+=j
+           # print(j)
+print(su,time.time()-t)
