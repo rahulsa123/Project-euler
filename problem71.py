4df610e4a1bbe7fbbdec86176718b006deee9fb7
Rahul sahu <43337390+rahulsa123@users.noreply.github.com>
2019-06-18 02:01:26 +0530

    Update problem71.py

--- problem71.py
+++ problem71.py
@@ -1,3 +1,16 @@
+"""
+first how to genrate all fraction is order
+       0/1                 1/1                   1/0  
+                                            (lets take it 1/0 then just find middle one using addition of numerator with numerator and same with deno.)
+       0/1    0+1/1+1=1/2      1/1    1+1/1+0 =2/1       0/1
+       
+       0/1   0+1/1+2=1/3   1/2    1+1/2+1=2/3   1/1  1+2/1+1=3/2  2/1     0/1
+
+and so on just calculate using variable
+
+"""
+
+
 x1=2
 x2=5
 g1=3
