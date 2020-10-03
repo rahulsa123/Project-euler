6f6cce89c6c8f06c2001bb6b9d649b93bfc311fb
Rahul Sahu <Rahul.et2016@gmail.com>
2020-10-03 05:46:04 +0530

    proble93 solved

--- problem93.py
+++ problem93.py
@@ -1,41 +1,46 @@
-import itertools
-bracket = [
-    " {} {} {} ",
-    "( {} ){} {} ",
-    "( {} {} ){} ",
-    " {}( {} ){} ",
-    " {}( {} {} )",
-    "( {} ){}( {} )",
-    "( {}( {} )){} ",
-    "(( {} ){} ){} ",
-    " {}(( {} ){} )",
-    " {}( {}( {} ))",
-]
-all_combination = []
-for i in itertools.permutations("+-*/", 3):
-    for config in bracket:
-        all_combination.append(config.format(*i).replace(" ", "{}"))
-# print(all_combination[:10],len(all_combination))
-max_found = 0
+"""
+(1, 2, 5, 8) 51
 
-for d in range(4, 100):
+real	0m1.729s
+user	0m1.723s
+sys	0m0.004s
+
+
+"""
+
+import itertools
+
+def cal(l,ref):
+	if len(l)==1:
+		if l[0]>0 and l[0] ==int(l[0]):
+			ref.add(l[0])
+		return
+	for pick in [(0,1),(1,2),(2,3)]:
+		if  pick[-1]>= len(l):
+			break
+		# here we select pick's element from l now apply all operation
+		# add operation
+		cal(l[:pick[0]]+[ (l[pick[0]] + l[pick[1]]) ] + l[pick[1]+1:], ref )		
+		# sub operation
+		cal(l[:pick[0]]+[ (l[pick[0]] - l[pick[1]]) ] + l[pick[1]+1:], ref )
+		# mul operation
+		cal(l[:pick[0]]+[ (l[pick[0]] * l[pick[1]]) ] + l[pick[1]+1:], ref )
+		# div operation
+		if l[pick[1]] !=0 : 
+			cal(l[:pick[0]]+[ (l[pick[0]] / l[pick[1]]) ] + l[pick[1]+1:], ref )
+max_found = 0
+result  = [0,0,0,0]
+for d in range(4, 10):
     for c in range(3, d):
         for b in range(2, c):
             for a in range(1, b):
-                ref = [0]*len(all_combination)
-                for com in all_combination:
-                    for config in itertools.permutations([str(x) for x in [a, b, c, d]]):
-                        com.format(*config)
-                        try:
-                            res = eval(com)
-                            if int(res) == res and res > 0:
-                                ref[int(res)] = 1
-                        except Exception:
-                            pass
-                if ref[1] != 1:
-                    continue
-                exit()
-                temp = ref[1:].index(0)
-                if max_found < temp:
-                    max_found = temp
-                    print(a, b, c, d, max_found)
+            	ref = set()
+            	for com in itertools.permutations([a,b,c,d]):
+            		cal(list(com),ref)
+            	temp = 0
+            	while(temp+1 in ref):
+            		temp+=1
+            	if max_found < temp:
+            		max_found = temp
+            		result = a,b,c,d
+print(result, max_found)            	
